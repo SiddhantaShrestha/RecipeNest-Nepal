e8abea748dfdd5fac003b9ed91627440b9830e46
Siddhanta Shrestha <siddhanta.shrestha2004@gmail.com>
2025-03-19 21:28:31 +0545

    order schema

--- backend/src/Schema/model.js
+++ backend/src/Schema/model.js
@@ -3,8 +3,10 @@ import registerSchema from "../Schema/userSchema.js";
 import blogSchema from "./blogSchema.js";
 import recipeSchema from "./recipeSchema.js";
 import categorySchema from "./categorySchema.js";
+import orderSchema from "./orderSchema.js";
 
 export let Register = model("Register", registerSchema);
 export let Blog = model("Blog", blogSchema);
 export let Recipe = model("Recipe", recipeSchema);
 export let Category = model("Category", categorySchema);
+export let Order = model("Order", orderSchema);
